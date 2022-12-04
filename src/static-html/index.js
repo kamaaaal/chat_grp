6e160fec9f71861fd2bd932916b52d0482010a48
devKamal <itskamalmohamed@gmail.com>
2022-12-04 12:30:03 +0530

    socket connection url changed

--- src/static-html/index.js
+++ src/static-html/index.js
@@ -1,23 +1,21 @@
 ////////chat app
-function addMessage(message){
-    const messageBox = document.querySelector('.message-box');
-    console.log('message to be added',message);
-    const mess = document.createElement('p');
-    mess.className = "message-sent";
-    mess.innerHTML = `[${message.name}] : ${message.message}` ;
-    messageBox.appendChild(mess);
+function addMessage(message) {
+  const messageBox = document.querySelector('.message-box');
+  console.log('message to be added', message);
+  const mess = document.createElement('p');
+  mess.className = 'message-sent';
+  mess.innerHTML = `[${message.name}] : ${message.message}`;
+  messageBox.appendChild(mess);
 }
 
 const UserName = prompt('Enter User Name');
 
-const socket = io("ws://192.168.43.194:3000/");
+const socket = io('https://chatgrp-production.up.railway.app/:3000/');
 
-socket.emit('userConnected',UserName);
+socket.emit('userConnected', UserName);
 
-socket.on('userConnected',(UserName) => {
-    console.log('got user name ',UserName);
-})
+socket.on('userConnected', (UserName) => {
+  console.log('got user name ', UserName);
+});
 
-
-socket.on('message',content => console.log(content));
-
+socket.on('message', (content) => console.log(content));
